6bad6dd49101567fced63e894f5c8b2b872a34fe
RJ Freund <robert.joseph.freund@gmail.com>
2015-07-13 19:10:44 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(){
+    router.get('/', function(req, res, next) {
+        res.render('index', { title: 'Socket-Chess' });
+    });
+    return router;
+};
+
